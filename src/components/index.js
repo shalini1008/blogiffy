2009859e46e4bfd2204e21600c3a4f3f547ba8e7
Shalini <122449558+shalini1008@users.noreply.github.com>
2024-05-30 14:43:50 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,6 @@ import { Header } from "./Header/Header";
 import { Footer } from "./Footer/Footer";
 import { Logo } from "./Logo";
 import { LogoutBtn } from "./Header/LogoutBtn";
-import { Footer } from "./footer/Footer";
-import { Logo } from "./Logo";
 import { Container } from "./Container"
 import { Button } from "./Button";
 import Input from "./Input";
