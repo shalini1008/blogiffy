34a26a88986bf36364f5db2edcaf1581e7c993c2
Shalini <122449558+shalini1008@users.noreply.github.com>
2024-05-30 14:10:12 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,9 @@ import { AllPost } from "./AllPost";
 import { EditPost } from "./EditPost";
 import { Home } from "./Home";
 import Post from "./Post";
-<<<<<<< HEAD
+
 import { LogIn } from "./LogIn";
-=======
-import { LogIn } from "./Login";
->>>>>>> 31e556cd5d76624749a72095fe6e33a69157b304
+
 import { Signup } from "./Signup";
 
 export {
