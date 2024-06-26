a9d25197e16e2183523d668648c02b88724d5564
Shalini <122449558+shalini1008@users.noreply.github.com>
2024-06-27 03:30:14 +0530

    Update auth.js

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -4,56 +4,56 @@ import { Client, Account, ID } from "appwrite";
 export class AuthService {
     client = new Client();
     account;
-
+        
     constructor() {
         this.client
             .setEndpoint(conf.appWriteUrl)
             .setProject(conf.appWriteProjectId);
         this.account = new Account(this.client);
-            
     }
 
-    async createAccount({email, password, name}) {
-         try {
-            const userAccount = await this.account.create(ID.unique(), email, password, name);
-            // If the user is successfully created, log them in
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, { name });
+    
             if (userAccount) {
-                return this.loginAccount({ email, password });
-            }
-            else{
-                return userAccount
+                // Log in the user after successful account creation
+                return this.login({ email, password });
+            } else {
+                throw new Error('Failed to create account');
             }
         } catch (error) {
             throw new Error(`Failed to create account: ${error.message}`);
         }
     }
+    
 
-    async login({email, password}) {
-         try {
-            return await this.account.createEmailPasswordSession(email,password);
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
         } catch (error) {
             throw new Error(`Failed to login: ${error.message}`);
         }
     }
 
     async getCurrentUser() {
-       try {
-            return await this.account.getSession('current');
+        try {
+            return await this.account.get();
         } catch (error) {
             throw new Error(`Failed to get account details: ${error.message}`);
         }
-        return null;
     }
 
     async logout() {
-
         try {
             await this.account.deleteSessions();
         } catch (error) {
             throw new Error(`Failed to logout: ${error.message}`);
         }
+    }
 }
 
 const authService = new AuthService();
 
-export default authService
+export default authService;
+
